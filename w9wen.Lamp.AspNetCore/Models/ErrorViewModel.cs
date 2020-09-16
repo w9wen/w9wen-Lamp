701a2540385aaaa369c881fba66db251936b069e
William (PC) <william.wen@gmail.com>
2020-09-17 06:20:50 +0800

    Add project files.

---
+++ w9wen.Lamp.AspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace w9wen.Lamp.AspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
