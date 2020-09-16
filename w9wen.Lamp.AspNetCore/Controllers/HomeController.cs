13b4b9ee694eaf76cbe249ba73e5a75c5323ffa3
William (PC) <william.wen@gmail.com>
2020-09-17 06:45:41 +0800

    Add some syncfusion AspNetCore components.

--- w9wen.Lamp.AspNetCore/Controllers/HomeController.cs
+++ w9wen.Lamp.AspNetCore/Controllers/HomeController.cs
@@ -20,6 +20,8 @@ namespace w9wen.Lamp.AspNetCore.Controllers
 
         public IActionResult Index()
         {
+            var Order = OrdersDetails.GetAllRecords();
+            ViewBag.DataSource = Order;
             return View();
         }
 
