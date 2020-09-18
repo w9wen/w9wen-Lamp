fb936177a03f1e792190c3feadb02a3c0638bba9
William (PC) <william.wen@gmail.com>
2020-09-19 07:44:51 +0800

    [Syncfusion] Provide the list data to DropDown using ViewBag.

--- w9wen.Lamp.AspNetCore/Controllers/HomeController.cs
+++ w9wen.Lamp.AspNetCore/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Syncfusion.EJ2.DropDowns;
 using w9wen.Lamp.AspNetCore.Models;
 
 namespace w9wen.Lamp.AspNetCore.Controllers
@@ -22,6 +23,44 @@ namespace w9wen.Lamp.AspNetCore.Controllers
         {
             var Order = OrdersDetails.GetAllRecords();
             ViewBag.DataSource = Order;
+
+
+            // string[] data = new string[] { "Austria", "Brazil", "Canada", "Cameroon", "Denmark", "Finland", "Germany", "Switzerland" };
+
+            // var data = new List<object>() { new { value = "test", text = "test" }, new { value = "test2", text = "test2" } };
+
+
+            var data = new List<object>()
+            {
+                new {Id = 0, Country = "Australia"},
+                new {Id = 1, Country = "Bermuda"},
+                new {Id = 2, Country = "Canada"},
+                new {Id = 3, Country = "Cameroon"},
+                new {Id = 4, Country = "Denmark"},
+                new {Id = 5, Country = "Finland"},
+                new {Id = 6, Country = "Greenland"},
+                new {Id = 7, Country = "Poland"},
+            };
+
+            // var dropDownList = new DropDownList()
+            // {
+            //     // DataSource = new Syncfusion.EJ2.DataManager()
+            //     // {
+            //     //     Url = "/api/Employee",
+            //     //     Adaptor = "WebApiAdaptor"
+            //     // },
+            //     DataSource = data,
+            //     Query = "new ej.data.Query()",
+            //     Fields = new Syncfusion.EJ2.DropDowns.DropDownListFieldSettings()
+            //     {
+            //         Value = "Id",
+            //         Text = "Country"
+            //     },
+            // };
+
+
+            // ViewBag.DropDownData = new { placeholder = "Find a country", dataSource = data };
+            ViewBag.DropdownData = data;
             return View();
         }
 
