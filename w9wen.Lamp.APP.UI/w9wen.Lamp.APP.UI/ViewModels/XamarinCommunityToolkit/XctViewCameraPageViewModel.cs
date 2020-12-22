3e470a19433a151c7a6d1b3168550c7a4b8a7d7d
William (NB) <william.wen@gmail.com>
2020-12-22 09:07:19 +0800

    [XCT] Capture image using CameraView.

--- w9wen.Lamp.APP.UI/w9wen.Lamp.APP.UI/ViewModels/XamarinCommunityToolkit/XctViewCameraPageViewModel.cs
+++ w9wen.Lamp.APP.UI/w9wen.Lamp.APP.UI/ViewModels/XamarinCommunityToolkit/XctViewCameraPageViewModel.cs
@@ -3,14 +3,38 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
 
 namespace w9wen.Lamp.APP.UI.ViewModels
 {
     public class XctViewCameraPageViewModel : BindableBase
     {
+        private ImageSource captureImageSource;
+
+        public ImageSource CaptureImageSource
+        {
+            get { return captureImageSource; }
+            set { SetProperty(ref captureImageSource, value); }
+        }
+
+        private DelegateCommand<MediaCapturedEventArgs> mediaCapturedCommand;
+
+        public DelegateCommand<MediaCapturedEventArgs> MediaCapturedCommand =>
+            mediaCapturedCommand ?? (mediaCapturedCommand = new DelegateCommand<MediaCapturedEventArgs>(async (a) => await ExecuteMediaCapturedCommand(a)));
+
+        private async Task ExecuteMediaCapturedCommand(MediaCapturedEventArgs e)
+        {
+            if (e != null &&
+                e.Image != null)
+            {
+                this.CaptureImageSource = e.Image;
+            }
+        }
+
         public XctViewCameraPageViewModel()
         {
-
         }
     }
 }
